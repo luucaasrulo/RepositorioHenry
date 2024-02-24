f3aa146359cb5377cad87f02ba61734be3a1365a
luucaasrulo <lucas___1997@hotmail.com>
2024-02-24 16:04:28 -0300

    .

--- C5 Fundamentos Javascript/ejercicios/08.js
+++ C5 Fundamentos Javascript/ejercicios/08.js
@@ -3,6 +3,13 @@ function esNumeroEntero(numero) {
   // Verifica si este es un número entero o no.
   // Returna true si lo es, de lo contrario, retorna false.
   // Tu código:
+  redondeado = Math.round(numero)
+  if (numero === redondeado) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 module.exports = esNumeroEntero;
